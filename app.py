f1ce351e6575d4e08b9d7bfdb908326b7f74320a
schellrw <schell.rw@gmail.com>
2024-07-24 22:21:31 -0500

    no key for this widget

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ with chat_placeholder:
 with prompt_placeholder:
     st.chat_input(
         "Chat", 
-        key="human_prompt",
+        ##  key="human_prompt",
         on_submit=on_click_callback
         )
     # st.form_submit_button(
