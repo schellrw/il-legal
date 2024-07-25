1f2d820330ed98ad6b1659ef5f75690bb7cc273b
schellrw <schell.rw@gmail.com>
2024-07-24 22:17:35 -0500

    no container args

--- app.py
+++ app.py
@@ -124,9 +124,9 @@ st.markdown(
     """
 )
 
-chat_placeholder = st.container("chat-container")
+chat_placeholder = st.container()
 # prompt_placeholder = st.form("chat-form")
-prompt_placeholder = st.container("prompt-container")
+prompt_placeholder = st.container()
 # prompt_placeholder = st.chat_input("Chat", key="human_prompt")
 
 with chat_placeholder:
